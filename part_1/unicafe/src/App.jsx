2fb3675809aed11806ec00aa706ee76c86314d11
Ufrian <ufrian.contato@gmail.com>
2023-09-06 10:59:05 -0300

    exercise 1.9 and 1.10

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -4,35 +4,43 @@ const Header = ({text}) => <h1>{text}</h1>
 
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
-const Statistic = ({text, value}) => <div>{text} {value}</div>
+const Statistics = ({value}) => {
+  const sumOfScores = value.good + value.neutral + value.bad
+  const avg = ( value.good - value.bad ) / sumOfScores
+  const positive = ( value.good / sumOfScores ) * 100
+
+  if (sumOfScores === 0) return (<div>No feedback given</div>)
+
+  return (
+    <div>
+      <StatisticsLine value={value.good} text="good" />
+      <StatisticsLine value={value.neutral} text="neutral" />
+      <StatisticsLine value={value.bad} text="bad" />
+      <StatisticsLine value={sumOfScores} text="all" />
+      <StatisticsLine value={avg} text="average" />
+      <StatisticsLine value={`${positive} %`} text="positive"/> 
+    </div>
+  )
+}   
+
+
+const StatisticsLine = ({text, value}) => <div>{text} {value}</div>
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const sumOfScores = good + neutral + bad
-  
-  const avg = (good - bad) / sumOfScores
-  const positive = ( good / sumOfScores ) * 100
-
   
   return (
     <div>
-      {/* Try later on! refactor and encapsule in a Feedback and a Statistic component */}
       <Header text="give feedback"/>
       <Button handleClick={() => setGood(good + 1)} text="good"/>
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
       <Button handleClick={() => setBad(bad + 1)} text="bad"/>
 
       <Header text="statistics"/>
-      <Statistic value={good} text="good" />
-      <Statistic value={neutral} text="neutral" />
-      <Statistic value={bad} text="bad" />
-      <Statistic value={sumOfScores} text="all" />
-      <Statistic value={avg} text="average" />
-      <Statistic value={`${positive} %`} text="positive"/> 
+      <Statistics value={{good, neutral, bad}}/>
     </div>
   )
 }
