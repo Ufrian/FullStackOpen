22472b46c5c859515f5d0a216c0e2a494fb8b504
Ufrian <ufrian.contato@gmail.com>
2023-09-08 14:19:08 -0300

    exercise 1.11*

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -4,6 +4,15 @@ const Header = ({text}) => <h1>{text}</h1>
 
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
+const StatisticsLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
 const Statistics = ({value}) => {
   const sumOfScores = value.good + value.neutral + value.bad
   const avg = ( value.good - value.bad ) / sumOfScores
@@ -12,22 +21,20 @@ const Statistics = ({value}) => {
   if (sumOfScores === 0) return (<div>No feedback given</div>)
 
   return (
-    <div>
+    <table>
+      <tbody>
       <StatisticsLine value={value.good} text="good" />
       <StatisticsLine value={value.neutral} text="neutral" />
       <StatisticsLine value={value.bad} text="bad" />
       <StatisticsLine value={sumOfScores} text="all" />
       <StatisticsLine value={avg} text="average" />
       <StatisticsLine value={`${positive} %`} text="positive"/> 
-    </div>
+      </tbody>
+    </table>
   )
 }   
 
-
-const StatisticsLine = ({text, value}) => <div>{text} {value}</div>
-
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -44,6 +51,5 @@ const App = () => {
     </div>
   )
 }
-
 
 export default App
