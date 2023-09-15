96c6192ac9daf9eb34b7c7afcce9b6e26a1ce425
Matheus <ufrian.contato@gmail.com>
2023-09-15 18:03:24 -0300

    exercise 2.5

--- part_2/courseinfo/src/App.jsx
+++ part_2/courseinfo/src/App.jsx
@@ -1,35 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = (props) => {
-  const { parts } = props
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-    
-  return (
-      <b>Number of exercises {total}</b>
-    )
-}
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-    <div>
-      {parts.map(part => <Part key={part.id} part={part} />)}
-    </div>
-    
-const Course = (props) => {
-  const { course } = props
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
@@ -79,6 +48,7 @@ const App = () => {
 
   return (
     <div>
+      <h1>Web development curriculum</h1>
       {courses.map((course) =>  <Course key={course.id} course={course} />)}
     </div>
   )
