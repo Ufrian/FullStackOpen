3d82d7e75befa05327f72513f8e12d035fac6e03
Matheus <ufrian.contato@gmail.com>
2023-10-21 10:44:36 -0300

    refactor countries to component module

--- part_2/countries/services/countries.js
+++ part_2/countries/services/countries.js
@@ -1,10 +1,18 @@
 import axios from 'axios'
 
 const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/all"
+const API_KEY = import.meta.env.VITE_API_KEY
+
+// api_call = https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
 const getAll = () => 
     axios
       .get(`${baseUrl}`)
       .then(response => response.data)
 
-export default {getAll}
+const getWeather = (lat, lon) =>
+  axios
+    .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+    .then(response => response.data)
+
+export default {getAll, getWeather}
--- part_2/countries/src/App.jsx
+++ part_2/countries/src/App.jsx
@@ -1,64 +1,11 @@
 import { useState, useEffect } from 'react'
 import countriesService from '../services/countries'
+import Countries from '../components/Countries'
 
 const Form = (props) => 
   <div>
     find countries: <input value={props.country} onChange={props.handleChange} />
   </div>
-
-const SingleCountry = ({country}) => {
-  const languages = Object.values(country.languages)
-
-  return (
-    <div>
-      <h1>{country.name.common}</h1>
-      <img src={country.flags.png}/>
-      <div>
-        Capital: {country.capital}
-      </div>
-      <div>
-        Area: {country.area}
-      </div>
-      <h3>languages</h3>
-      <ul>
-        {languages.map(lang => <li key={lang}>{lang}</li>)}
-      </ul>
-    </div>
-  )
-}
-
-const Countries = ({countries, handleShow, isShow}) => {
-  if (countries.length > 10) return (
-    <div>Too many matches, specify another filter</div>
-  )
-
-  if (countries.length === 1) return (
-    <div>
-      <SingleCountry country={countries[0]} />
-    </div>
-  )
-
-  return (
-    <div>
-      {countries.map(country => 
-        <Country key={country.name.official} country={country} handleShow={handleShow} isShow={isShow} />  
-      )}
-    </div>
-  )
-}
-
-const Country = ({country, handleShow, isShow}) => {
-  const countryStatus = isShow.filter(id => Object.keys(id)[0] === country.name.official)  
-  const btnTxt = countryStatus[0] === undefined ? "show" : (countryStatus[0][country.name.official] ? "hide" : "show") 
-
-  return (
-    <div>
-      {country.name.common}
-    <button type="submit" onClick={() => handleShow(country.name.official)} >{btnTxt}</button>
-    {btnTxt === "hide" ? <SingleCountry country={country} /> : null } 
-    </div>
-  )
-}
 
 const App = () => {
   const [country, setCountry] = useState('')
@@ -68,10 +15,10 @@ const App = () => {
 
   useEffect (() => {
       countriesService
-      .getAll()
-      .then(CountriesData => setAllCountriesData(CountriesData))
+       .getAll()
+       .then(CountriesData => setAllCountriesData(CountriesData))
   }, [])
-  
+
   const handleCountryChange = (e) => {
     if (e.target.value) setShowCountries(true)
     else setShowCountries(false) 
@@ -89,14 +36,13 @@ const App = () => {
     }
 
     else setShowStatus(copyStatus.concat({[id]: true}))
-  } 
-
+  }
+  
   if (!allCountriesData) return
   
   const filterCountries = () => allCountriesData.filter(countryObj => countryObj.name.common.toLowerCase().includes(country.toLowerCase()))
 
   const filteredCountries = showCountries ? filterCountries() : []
-
 
   return (
     <div>
