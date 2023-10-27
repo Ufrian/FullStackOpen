12a3429dfd17ca37b3eca88960b463d6e5b8fdc0
Matheus <ufrian.contato@gmail.com>
2023-10-27 20:09:07 -0300

    exercise 3.5

--- part_3/phonebook_backend/index.js
+++ part_3/phonebook_backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -55,6 +57,26 @@ app.delete('/api/persons/:id', (request, response) => {
 
     response.status(204).end()
 })
+
+const generateId = () => {
+  return Math.floor(Math.random() * Date.now() % 10000) 
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) return response.status(400).json({error: "content missing"})
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+})
+  
 
 const PORT = 3001
 app.listen(PORT, () => {
