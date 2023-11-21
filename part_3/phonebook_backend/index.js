a8040278cc32eb821cec97551de665598a16781e
Matheus <ufrian.contato@gmail.com>
2023-11-21 16:03:49 -0300

    exercise 3.16

--- part_3/phonebook_backend/index.js
+++ part_3/phonebook_backend/index.js
@@ -9,8 +9,6 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
 
-let persons = []
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
@@ -20,7 +18,7 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     Person.countDocuments({}).then(count => {
       const date = new Date().toString()
       const html = `
@@ -31,25 +29,34 @@ app.get('/info', (request, response) => {
       `
       response.send(html)
     })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-      response.json(person)
+      if (person) response.json(person)
+      else response.send(404).end()
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
   .then(result => {
     response.status(204).end()
   })
+  .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (!body.name || !body.number) return response.status(400).json({error: "name/number missing"})
+  if (!body.name || !body.number) {
+    const err = new Error('Failed to add! Missing name/number fields')
+    err.name = "MissingField"
+    next(err)
+    return 
+  }
 
   const person = new Person({
     name: body.name,
@@ -59,7 +66,25 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === "CastError") {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+
+  if (error.name === "MissingField") {
+    return response.status(400).json({error: "name/number missing"})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
