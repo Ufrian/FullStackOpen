33a77245ca7cef78510eae72a4ae1e3bd60bff13
Matheus <ufrian.contato@gmail.com>
2023-12-12 14:30:17 -0300

    Part4a unit testing

--- course/notes_backend/.eslintrc.js
+++ course/notes_backend/.eslintrc.js
@@ -1,38 +1,39 @@
 module.exports = {
-    'env': {
-        'commonjs': true,
-        'es2021': true,
-        'node': true
-    },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'ecmaVersion': 'latest'
-    },
-    'rules': {
-        'indent': [
-            'error',
-            2,
-        ],
-		'eqeqeq': 'error',
-		'no-trailing-spaces': 'error',
+  'env': {
+    'commonjs': true,
+    'es2021': true,
+    'node': true,
+    'jest': true,
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2,
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
     'object-curly-spacing': [
-        'error', 'always'
+      'error', 'always'
     ],
     'arrow-spacing': [
-        'error', { 'before': true, 'after': true }
+      'error', { 'before': true, 'after': true }
     ],
-	'no-console': 0,
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
-    }
+    'no-console': 0,
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
 }
