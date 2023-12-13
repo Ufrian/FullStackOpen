7e8e2c81b24bf0eb159ddf6e46ccc658a0fa8240
Matheus <ufrian.contato@gmail.com>
2023-12-13 10:30:23 -0300

    exercise 4.5*

--- part_4/blog_list/utils/list_helper.js
+++ part_4/blog_list/utils/list_helper.js
@@ -7,8 +7,18 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const fav = blogs.reduce((prev, blog) => prev.likes > blog.likes ? prev : blog)
+
+  return {
+    title: fav.title,
+    author: fav.author,
+    likes: fav.likes
+  }
+}
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
