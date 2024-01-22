953727dcb69e8b12653931d1476ff38a37cdedb1
Matheus <ufrian.contato@gmail.com>
2024-01-22 10:20:21 -0300

    exercise 4.10

--- part_4/blog_list/tests/list_api.test.js
+++ part_4/blog_list/tests/list_api.test.js
@@ -32,6 +32,27 @@ test('unique identifier property is named id', async () => {
   expect(firstBlog.id).toBeDefined()  
 })
 
+test('a blog can be added', async () => {
+  const newBlog = {
+    title: "How to Learn from Tutorials the Right Way - and Not Get Trapped in Tutorial Hell",
+    author: "Jessica Wilkins",
+    url: "https://www.freecodecamp.org/news/how-to-learn-from-coding-tutorials-and-avoid-tutorial-hell/",
+    likes: 19
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(b => b.title)
+  expect(titles).toContain('How to Learn from Tutorials the Right Way - and Not Get Trapped in Tutorial Hell')
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
