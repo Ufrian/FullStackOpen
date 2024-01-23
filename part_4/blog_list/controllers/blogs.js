1dd96490bdb31f2ddd007083faee0a366b8edbc2
Matheus <ufrian.contato@gmail.com>
2024-01-23 11:07:15 -0300

    exercise 4.13

--- part_4/blog_list/controllers/blogs.js
+++ part_4/blog_list/controllers/blogs.js
@@ -6,11 +6,23 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+
+  if (blog) response.json(blog)
+  else response.status(404).end()
+})
+
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
   const result = await blog.save()
   response.status(201).json(result)
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
--- part_4/blog_list/tests/list_api.test.js
+++ part_4/blog_list/tests/list_api.test.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const helper = require('./test_helper')
 const app = require('../app')
 const Blog = require('../models/blog')
+const blogsRouter = require('../controllers/blogs')
 
 const api = supertest(app)
 
@@ -11,93 +12,119 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as JSON format', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+describe('when there are initially some notes saved', () => {
+  test('blogs are returned as JSON format', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
   
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
-test('unique identifier property is named id', async () => {
-  const blogs = await helper.blogsInDb()
-
-  const firstBlog = blogs[0]   
-
-  expect(firstBlog.id).toBeDefined()  
+describe('viewing a specific blog', () => {
+  test('unique identifier property is named id', async () => {
+    const blogs = await helper.blogsInDb()
+    
+    const firstBlog = blogs[0]   
+    expect(firstBlog.id).toBeDefined()  
+  })
 })
 
-test('a blog can be added', async () => {
-  const newBlog = {
-    title: "How to Learn from Tutorials the Right Way - and Not Get Trapped in Tutorial Hell",
-    author: "Jessica Wilkins",
-    url: "https://www.freecodecamp.org/news/how-to-learn-from-coding-tutorials-and-avoid-tutorial-hell/",
-    likes: 19
-  }
-
-  await api
+describe('addition of a blog', () => {
+  test('succeeds with valid data', async () => {
+    const newBlog = {
+      title: "How to Learn from Tutorials the Right Way - and Not Get Trapped in Tutorial Hell",
+      author: "Jessica Wilkins",
+      url: "https://www.freecodecamp.org/news/how-to-learn-from-coding-tutorials-and-avoid-tutorial-hell/",
+      likes: 19
+    }
+    
+    await api
     .post('/api/blogs')
     .send(newBlog)
     .expect(201)
     .expect('Content-Type', /application\/json/)
   
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    
+    const titles = blogsAtEnd.map(b => b.title)
+    expect(titles).toContain('How to Learn from Tutorials the Right Way - and Not Get Trapped in Tutorial Hell')
+  })
 
-  const titles = blogsAtEnd.map(b => b.title)
-  expect(titles).toContain('How to Learn from Tutorials the Right Way - and Not Get Trapped in Tutorial Hell')
-})
-
-test('if likes property is missing, it will default to the value 0', async () => {
-  const newBlog = {
-    title: "5 Common Server Vulnerabilities with Node.js",
-    author: "JavaScript Today",
-    url: "https://blog.javascripttoday.com/blog/node-js-server-vulnerabilities/",
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  const [lastBlog] = blogsAtEnd.slice(-1)
+  test('if likes property is missing, default value to 0', async () => {
+    const newBlog = {
+      title: "5 Common Server Vulnerabilities with Node.js",
+      author: "JavaScript Today",
+      url: "https://blog.javascripttoday.com/blog/node-js-server-vulnerabilities/",
+    }
   
-  expect(lastBlog.likes).toBe(0)
-})
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+  
+    const [lastBlog] = blogsAtEnd.slice(-1)
+    expect(lastBlog.likes).toBe(0)
+  })
+  
+  test('fail with status code 400 if no title property', async () => {
+    const noTitleBlog = {
+      author: "The Fortnite Team",
+      url: "https://www.fortnite.com/competitive/news/fortnite-championship-series-2024-details",
+      likes: 20
+    }  
+  
+    await api
+      .post('/api/blogs')
+      .send(noTitleBlog)
+      .expect(400)
 
-test('verify if title or url properties are missing from request', async () => {
-  const noTitleBlog = {
-    author: "The Fortnite Team",
-    url: "https://www.fortnite.com/competitive/news/fortnite-championship-series-2024-details",
-    likes: 20
-  }
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)  
+  })
 
-  await api
-    .post('/api/blogs')
-    .send(noTitleBlog)
-    .expect(400)
+  test('fail with status code 400 if no url property', async () => {
+    const noUrlBlog = {
+      title: "Oracle introduces JavaScript support in MySQL",
+      author: "Paul Krill",
+      likes: 1
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(noUrlBlog)
+      .expect(400)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)  
+  })
+})  
 
-  const noUrlBlog = {
-    title: "Oracle introduces JavaScript support in MySQL",
-    author: "Paul Krill",
-    likes: 1
-  }
+describe('deletion of a blog', () => {
+  test('valid id succeeds with status code 204', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
-  await api
-    .post('/api/blogs')
-    .send(noUrlBlog)
-    .expect(400)
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
 
+    const blogs = blogsAtEnd.map(b => b.title)
+    expect(blogs).not.toContain(blogToDelete.title)
+  })
 })
 
 afterAll(async () => {
