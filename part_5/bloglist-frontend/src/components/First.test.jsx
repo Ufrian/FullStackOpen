7c07f2d1b454271436c25cc9fd73b19aaf4b5f64
Matheus <ufrian.contato@gmail.com>
2024-03-15 14:52:37 -0300

    exercise 5.14

--- part_5/bloglist-frontend/src/components/First.test.jsx
+++ part_5/bloglist-frontend/src/components/First.test.jsx
@@ -1,16 +1,20 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-
 describe('Blog List Tests', () => {
-  test('<Blog/> Display title and author but not likes or URL', () => {
-    const blog = {
-      title: 'Why Bloat Is Still Softwares Biggest Vulnerability',
-      author: 'BERT HUBERT',
-      url: 'https://spectrum.ieee.org/lean-software-development',
-      likes: 2
-    }
+  const blog = {
+    title: 'Why Bloat Is Still Softwares Biggest Vulnerability',
+    author: 'BERT HUBERT',
+    url: 'https://spectrum.ieee.org/lean-software-development',
+    likes: 2,
+    user: {
+      username: 'pekora',
+      name: 'Usada Pekora',
+    },
+  }
 
+  test('<Blog/> Display title and author but not likes or URL', () => {
     const { container } = render(<Blog blog={blog}/>)
     const div = container.querySelector('.blog-display')
 
@@ -19,6 +23,19 @@ describe('Blog List Tests', () => {
 
     expect(div).not.toHaveTextContent(blog.url)
     expect(div).not.toHaveTextContent(blog.likes.toString())
+  })
+
+  test('Likes and URL are displayed after button clicked', async () => {
+    const toggleBtn = vi.fn()
+    const user = userEvent.setup()
+
+    render(<Blog blog={blog} toggleBtn={toggleBtn} />)
+
+    const btn = screen.getByRole('button')
+    await user.click(btn)
+
+    expect(screen.getByText(blog.url)).toBeDefined()
+    expect(screen.getByText(`likes: ${blog.likes}`)).toBeDefined()
   })
 })
 
