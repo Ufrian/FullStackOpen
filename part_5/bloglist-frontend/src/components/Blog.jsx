8b073cd5f750175535163bfe703b3a682326b294
Matheus <ufrian.contato@gmail.com>
2024-08-15 14:03:24 -0300

    Exercise 5.7

--- part_5/bloglist-frontend/src/components/Blog.jsx
+++ part_5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>  
-)
+import "./Blog.css"
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false)
+
+  const showView = { display: view ? '' : 'none' }
+
+  const toggleBtnView = () => {
+    setView(!view)
+  }
+  
+  return (
+  <div className="blog-view">  
+    <div>
+      {blog.title} - {blog.author} 
+    <button onClick={toggleBtnView} className="btn-view">{view ? "hide" : "view"}</button>  
+    </div>
+    <div style={showView}>
+      <div>
+        {blog.url}
+      </div>
+      <div>
+        likes: {blog.likes}
+        <button>like</button>
+      </div>
+      <div>
+        {blog.author}
+      </div>
+    </div>
+  </div>
+  )
+}
 
 export default Blog
