ddc478f64d0783d196e50fae484b2c954f701c4c
Matheus <ufrian.contato@gmail.com>
2024-09-02 15:32:21 -0300

    Exercise 5.14

--- part_5/bloglist-frontend/src/components/Blog.test.jsx
+++ part_5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react"
 import Blog from "./Blog"
-import { beforeEach, describe, expect } from "vitest"
+import userEvent from "@testing-library/user-event"
+import { useDeferredValue } from "react"
+import { expect } from "vitest"
 
 
 describe("<Blog />", () => {
@@ -16,19 +18,28 @@ describe("<Blog />", () => {
         name: "Wolfgang"
       }
     }
-    const container = render(<Blog blog={ blog } />).container
+    container = render(<Blog blog={ blog } />).container
   })
 
   test("renders title and author by default but dont likes and url", () => {
-
-
     const div = container.querySelector(".blog-header")
 
     expect(div).toHaveTextContent("Foobar the blog")
     expect(div).toHaveTextContent("J.R.R Martin")
     expect(div).not.toHaveTextContent("www.google.com.br")
     expect(div).not.toHaveTextContent("likes", { exact: false })
+  })
 
+  test("Numbers of likes and url are shown after button is clicked", async () => {
+    const user = userEvent.setup()
+    const btn = screen.getByText("view")
+
+    await user.click(btn)
+
+    const div = container.querySelector(".blog-view")
+
+    expect(div).toHaveTextContent("www.google.com.br")
+    expect(div).toHaveTextContent("likes: 10")
   })
 })
 
