d34c863f7d53262c0405334c0a6c3a2454378c79
Matheus Henrique <ufrian.contato@gmail.com>
2024-09-20 11:18:55 -0300

    Exercise 6.2

--- part_6/unicafe-redux/src/reducer.js
+++ part_6/unicafe-redux/src/reducer.js
@@ -5,7 +5,7 @@ const initialState = {
 }
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
+  // console.log(action)
   switch (action.type) {
     case 'GOOD':
       return {
@@ -30,8 +30,7 @@ const counterReducer = (state = initialState, action) => {
         ok: 0
       }
     default: return state
-  }
-  
+  } 
 }
 
 export default counterReducer
